7cfb37e802d1778e8e6f37cab7bdf50b80b7c813
Tony Yang <tony.tyss@gmail.xom>
2022-11-02 19:35:15 +0100

    saus

--- main.py
+++ main.py
@@ -40,8 +40,9 @@ T_ij = {(0, 1): 6, (0, 2): 8, (0, 3): 12, (0, 4): 100, (0, 5): 6.100, (0, 6): 7.
         (6, 0): 100, (6, 1): 100, (6, 2): 100, (6, 3): 15, (6, 4): 8, (6, 5): 4, (6, 7): 5, (7, 0): 100, (7, 1): 100, (7, 2): 100, (7, 3): 100, (7, 4): 6, (7, 5): 100, (7, 6): 5}
 #T_ij = {(i,j): np.hypot(xc[i]-xc[j], yc[i] - yc[j]) for i,j in A_k}
 Q_k = {i: 4 for i in range(nr_drivers)}
-A_k1 = {0:30, 1:30, 2: 15, 3: 30}
-A_k2 = {0:60, 1:45, 2: 30, 3: 45}
+A_k1 = {4:30, 5:30, 6: 15, 7: 30}
+A_k2 = {4:60, 5:45, 6: 30, 7: 45}
+M=100000
 
 
 model=Model('RRP')
@@ -52,16 +53,32 @@ x = model.addVars(pairs, vtype=GRB.BINARY, name ='x_kij')
 z = model.addVars(NP, vtype=GRB.BINARY, name='z_i')
 t = model.addVars(driver_node, vtype=GRB.CONTINUOUS, name='t_ki')
 
+model.modelSense = GRB.MINIMIZE
 model.setObjective(quicksum(T_ij[i,j]*x[k,i,j] for i, j in A_k for k in D))
 
 #routing constraints
 model.addConstrs(quicksum(x[k,i,j] for j in NP + [7]) == 1 for i in o_k for k in D)
 model.addConstrs(quicksum(x[k,i,j] for i in [0] + ND) == 1 for j in d_k for k in D)
 model.addConstrs((quicksum(x[k,i,j] for i in N_k[1:]) == quicksum(x[k,i,j] for i in N_k[:-1])) for k in D for j in N_k)
-model.addConstrs((quicksum()))
+model.addConstrs((quicksum(x[k,i,j] for i in N_k) - quicksum(x[k,nr_passengers+i,j] for i in N_k))==0 for k in D for j in N_k)
+model.addConstrs((quicksum(x[k,i,j] for k in D for j in N_k))-z[i]==0 for i in NP)
 
+#precedence constraint
+model.addConstrs(t[k,i] + T_ij[i, nr_passengers+i] - t[k, nr_passengers+i] <= 0 for k in D for i in NP)
 
+#time constraint
+model.addConstrs(t[k,i]+T_ij[i,j] - t[k,j] - M*(1-x[k,i,j]) <= 0 for k in D for i in N for j in N)
+model.addConstrs(A_k1[nr_passengers+i]<=t[k,nr_passengers+i]<=A_k2[nr_passengers+i] for k in D for i in NP)
+model.addConstrs(A_k1[k1]<=t[k, k1]<=A_k2[k1] for k in D for k1 in d_k)
 
+model.addConstrs(t[k,nr_passengers+i] - t[k,i] <= T_k[i] for k in D for i in NP)
+model.addConstrs(t[k,k1] - t[k, k2] <= T_k[k] for k in D for k1 in d_k for k2 in o_k)
+
+#capacity constraints
+
+model.addConstrs(quicksum(x[k,i,j] for i in NP for j in N_k) <= Q_k[k] for k in D)
+
+model.Params.MITGap=0.1
 
 
 

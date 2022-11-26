3b89378be4fe567d962811469c8e444c5ce8d315
Tony Yang <tony.tyss@gmail.xom>
2022-11-26 21:43:47 +0100

    Nice

--- main.py
+++ main.py
@@ -539,6 +539,8 @@ def run_only_once():
     #get_feasible_variables()
     arcs = visualize()
     print("FITTESAUS")
+    print("SATANIST")
+    print("KUKSAUS")
     return arcs
 
 def run_pareto():
@@ -546,6 +548,6 @@ def run_pareto():
 
 
 
-#run_only_once()
+run_only_once()
 
 

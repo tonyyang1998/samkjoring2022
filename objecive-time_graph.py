45a68d84a294933cac0062b8c568e27eb3c5df12
Shusheng Yang <shusheny@ntnu.no>
2022-12-06 14:35:50 +0100

    Correct instances and results

--- objecive-time_graph.py
+++ objecive-time_graph.py
@@ -1,19 +1,41 @@
 import matplotlib.pyplot as plt
 
+objVal = 9
 
-result_solution = [-1e+100, 2.0, 5.0, 7.0, 9.0, 11.0, 12.0]
-result_bound = [13.0, 13.0, 13.0, 13.0, 13.0, 13.0, 13.0]
-result_time = [0.16000008583068848, 0.17300009727478027, 0.5250000953674316, 0.8350000381469727, 1.948000192642212, 3.3000001907348633, 5.150000095367432]
+result_solution = [0, 2.0, 4.0, 5.0, 6.0, 9.0]
+result_bound = [10.0, 10.0, 10.0, 10.0, 10.0]
+result_time = [0.16000008583068848, 0.17300009727478027, 0.5250000953674316, 0.8350000381469727, 1.948000192642212, 3.3000001907348633]
 
-upper_bound=result_bound[0]
-x_axis=[]
 
-for i in range(3600):
-    x_axis.append(i)
+
+if objVal == result_bound[-1]:
+    for i in range(len(result_solution), 3600):
+        result_solution.append(result_bound[-1])
+    for i in range(len(result_time), 3600):
+        result_time.append(i)
+
+
+if objVal != result_bound[-1]:
+    last_solution = result_solution[-1]
+    print(last_solution)
+    for i in range(len(result_solution), 3600):
+        result_solution.append(last_solution)
+    for i in range(len(result_time), 3600):
+        result_time.append(i)
 
 def get_graph():
+    plt.figure(2)
+    plt.xlim(-160, 3800)
+    plt.ylim(-1, result_bound[-1] + 1)
+    plt.axhline(y = result_bound[0], color = 'r', linestyle = 'dashed', label = "UB = " + str(result_bound[-1]))
+    plt.axvline(x = 3600, color = 'y', linestyle = 'dashed', label = "Time = 3600s" )
     plt.plot(result_time, result_solution)
     plt.ylabel('Passengers served')
     plt.xlabel('Time [s]')
+    plt.legend()
+    
+    plt.legend(bbox_to_anchor = (0.85, 1.2), loc = 'upper center')
+    
     plt.show()
-get_graph
+
+get_graph()
